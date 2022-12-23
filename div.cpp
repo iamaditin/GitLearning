d7a8237a9abe12be2df935bfd786b6406220163f
Aditi <aditinasikkar2407@gmail.com>
2022-12-24 00:20:45 +0530

    div mul committed

---
+++ div.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=8;int b=2;
+	cout<<a/b<<endl;
+}
---
+++ multiply.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=8;int b=2;
+	cout<<"Multiplication = "<<a*b<<endl;
+}
